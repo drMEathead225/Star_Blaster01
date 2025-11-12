bfd5872ae8180a6435cf74e552b838176027a0ce
drMEathead225 <hollow269@morrisville.edu>
2025-11-12 13:35:23 -0500

    gg

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,36 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] float moveSpeed = 10f;
+    InputAction moveAction;
+
+    Vector3 moveVector;
+    Vector2 minBounds;
+    Vector2 maxBounds;
+
     void Start()
     {
-        
+        moveAction = InputSystem.actions.FindAction("Move");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        MovePlayer();
+    }
+    
+    void IntBounds()
+    {
+        Camera mainCamera = Camera.main;
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
+    }
+    
+    void MovePlayer()
+    {
+        moveVector = moveAction.ReadValue<Vector2>();
+
+        transform.position += moveVector * moveSpeed * Time.deltaTime;
     }
 }
